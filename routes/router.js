c2c4bef4761b32707b79009efc13b50e5e0d63ad
mbroumsadja <mbroumsadjaa@gmail.com>
2025-01-01 22:57:34 +0100

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -13,7 +13,7 @@ router.get('/', async (req,res)=> {
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/')
+    cb(null, 'public/uploads/')
   },
    filename: (req, file, cb) => {
    cb(null, Date.now() + path.extname(file.originalname))
